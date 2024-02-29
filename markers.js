37689fab5198d6ab6bd306f43519f6b45af46097
Julian Murphy <njmdistrisoft@gmail.com>
2024-02-29 09:06:57 -0300

    about

--- markers.js
+++ markers.js
@@ -208,6 +208,22 @@ function processMarkers(map, results) {
   });
 }
 
+function addAbout(map) {
+  var filterLayer = L.Control.extend({
+    onAdd: function (_map) {
+      var div = L.DomUtil.create("div", "leaflet-control-layers about-button");
+      div.innerHTML += `<a href="#" id="aboutButton"><strong>Sobre hongos.julianmurphy.ar</strong></a>`;
+      return div;
+    },
+  });
+
+  new filterLayer().addTo(map);
+
+  document.getElementById("aboutButton").addEventListener("click", () => {
+    MicroModal.show("about");
+  });
+}
+
 /**
  * execute once, prepare data
  */
@@ -223,6 +239,7 @@ async function processData(map, results) {
   currentData = fetchedData;
   processMarkers(map, fetchedData);
   addFilters(map, fetchedData);
+  addAbout(map);
 }
 
 function goToProcess(map) {
